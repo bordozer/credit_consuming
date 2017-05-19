cfb8f84be12d3e4d47dfb1d90afe4e927fbd3908
Borys Lukianov <Borys_Lukianov@epam.com>
2017-05-19 13:48:58 +0300

    Spring Integration demo - spring reactor - 4

--- modules/bank/src/main/java/com/blu/credit/conclusion/reactor/services/PoliceServiceImpl.java
+++ modules/bank/src/main/java/com/blu/credit/conclusion/reactor/services/PoliceServiceImpl.java
@@ -1,8 +1,11 @@
 package com.blu.credit.conclusion.reactor.services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.blu.integration.model.Applicant;
+import com.blu.integration.model.ClientType;
 import com.blu.integration.model.PoliceResponse;
 
 import lombok.RequiredArgsConstructor;
@@ -14,12 +17,21 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class PoliceServiceImpl implements PoliceService {
 
+    private final RestTemplate restTemplate;
+
+    @Value("${police.url}")
+    private String policeUrl;
+
     @Override
     public Mono<PoliceResponse> process(final Applicant applicant) {
         return Mono.fromCallable(() -> buildPoliceResponse(applicant));
     }
 
     private PoliceResponse buildPoliceResponse(final Applicant applicant) {
+        final PoliceResponse policeResponse = restTemplate.postForObject(policeUrl, applicant, PoliceResponse.class);
+        if (ClientType.CRIMINAL.equals(policeResponse.getClientType())) {
+            return PoliceResponse.criminal(applicant);
+        }
         return PoliceResponse.respectable(applicant);
     }
 }
